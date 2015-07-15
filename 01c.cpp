4b02ff01a0d5ac9b3ca3f45237af82e3c20f5a1c
WangChihHung <banjohn729@gmail.com>
2015-07-15 15:46:31 +0900

    first

---
+++ 01c.cpp
@@ -0,0 +1,18 @@
+/*‰Û‘è01B*/
+#include <stdio.h>
+
+int main(void)
+{
+	int a=10;
+	int b=20;
+	int c;
+	double d;
+	
+	c=a+b;
+	d=(a+b)/2.0;
+	
+	printf("c:%d\n",c);
+	printf("d:%f\n",d);
+	
+	return 0;
+}
